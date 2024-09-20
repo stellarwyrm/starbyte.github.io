01c1fa2e7bf9f9d8cf928b7c0ba6d37d34cb49ba
stellarwyrm <rhythmbyte@gmail.com>
2024-09-20 02:14:34 -0700

    Fixed modes working (hopefully) and added cookies

--- js/interact.js
+++ js/interact.js
@@ -1,3 +1,19 @@
+// Generic cookie function from https://stackoverflow.com/a/15724300
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function setThemeCookie(mode) {
+    if (cookieEnabled) {
+        document.cookie = "theme=" + mode + ";max-age="
+            + 60 * 60 * 24 * 7;
+    }
+}
+
+
+
 
 function toggleScheme() {
     // disable default setting
@@ -5,13 +21,20 @@ function toggleScheme() {
 
     // then set based on color-scheme variable
     if (document.documentElement.style.getPropertyValue("color-scheme").localeCompare("light") == 0) {
+        // set dark mode
+        setThemeCookie("dark")
+        document.documentElement.style.setProperty("--brightness-val", "70%");
         document.documentElement.style.setProperty("color-scheme", "dark");
-        document.getElementById("darkToggle").src="images/button_ld_light.svg";
+        document.getElementById("darkToggle").src="images/utility/button_ld_light.svg";
     } else {
+        // set light mode
+        setThemeCookie("light");
+        document.documentElement.style.setProperty("--brightness-val", "150%");
         document.documentElement.style.setProperty("color-scheme", "light");
-        document.getElementById("darkToggle").src="images/button_ld_dark.svg";
+        document.getElementById("darkToggle").src="images/utility/button_ld_dark.svg";
     }
 }
+
 
 // From https://www.w3schools.com/howto/howto_js_shrink_header_scroll.asp ;)
 // window.onscroll = function() {scrollFunction()};
---
+++ js/preload.js
@@ -0,0 +1,63 @@
+// Based on https://www.geeksforgeeks.org/how-to-create-cookie-consent-box-using-html-css-and-javascript/
+
+const consentBox = 
+    document.getElementById("consentBox");
+const acceptBtn = 
+    document.querySelector(".okCookie");
+const rejectBtn = 
+    document.querySelector(".noCookie");
+
+acceptBtn.onclick = () => {
+    document.cookie = "bakedby=swyrm;max-age="
+        + 60 * 60 * 24 * 7;
+    if (document.cookie) {
+        consentBox.classList.add("hide");
+        cookieEnabled = true;
+        // save theme
+        mode = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)? "dark":"light";
+        document.cookie = "theme="+mode+";max-age="
+            + 60 * 60 * 24 * 7;
+    } else {
+        alert
+            ("Cookie couldn't be set. Try"+
+              " unblocking this site from the cookie"+
+              " setting of your browser.");
+    }
+};
+
+rejectBtn.onclick = () => {
+    consentBox.classList.add("hide");
+};
+
+let checkCookie = 
+    document.cookie.indexOf("bakedby=swyrm");
+if (checkCookie !== -1) {
+    // Cookies found
+    consentBox.classList.add("hide");
+
+    // set theme
+    if(getCookie("theme").localeCompare("dark") == 0){
+        document.documentElement.style.setProperty("--brightness-val", "70%");
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        document.getElementById("darkToggle").src="images/utility/button_ld_light.svg";
+    } else {
+        document.documentElement.style.setProperty("--brightness-val", "150%");
+        document.documentElement.style.setProperty("color-scheme", "light");
+        document.getElementById("darkToggle").src="images/utility/button_ld_dark.svg";
+    }
+} else {
+    consentBox.classList.remove("hide");
+    // Loosely based on https://stackoverflow.com/a/57795495
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // dark mode
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        document.documentElement.style.setProperty("--brightness-val", "70%");
+    } else {
+        console.log("No cookie found!");
+        document.getElementById("preferMode").media='none';
+        document.documentElement.style.setProperty("color-scheme", "light");
+        document.documentElement.style.setProperty("--brightness-val", "150%");
+    }
+}
+    
+let cookieEnabled = (checkCookie !== -1 ) ? true : false;
